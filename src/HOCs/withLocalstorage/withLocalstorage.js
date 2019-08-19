4c7a146ff190adde324999bee2c83f2f9c8382b2
Atomilin <50828840+Atomilin@users.noreply.github.com>
2019-08-19 13:34:58 +0300

    homework_52

--- src/HOCs/withLocalstorage/withLocalstorage.js
+++ src/HOCs/withLocalstorage/withLocalstorage.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
 import { load, save } from '../../localstorage';
 
-const withLocalstorage = () => () => {};
+const withLocalstorage = (key, defaultData) => (WrappedComponent) => {
+    return class HOCLocalStorage extends Component {
+        constructor(props) {
+            super(props);
+
+            this.key = key;
+            this.data = load(key);
+            this.state = {
+                newData: []
+            };
+        }
+        
+        loadData = () => {
+            return load(key) || defaultData
+        };
+
+        updateData = (data) => {
+            save(key, data);
+            this.setState({
+                newData: load(key)
+            })
+        };
+
+        render() {
+            return <WrappedComponent savedData={this.loadData()} updateData={this.updateData}/>
+        }
+    }
+};
+
 
 export default withLocalstorage;
